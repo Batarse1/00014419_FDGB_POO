e5ae6cd2dd01054cb27d31725b0f90cddb6c60b3
Batarse1 <00014419@uca.edu.sv>
2020-04-23 21:53:26 -0600

    Sesión de Meet 4/23/2020

---
+++ Meet/Meet2304/Meet2304/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Permissions;
+
+namespace Meet2304
+{
+  internal class Program 
+  {
+    public static void Main(string[] args)
+    {
+      //List<Persona> p = new List<Persona>();
+      var lista = new List<Persona>();
+      lista.Add(new Persona(nombre:"Walter", apellido:"Rafael", edad: 20));
+      lista.Add(new Persona("Batar", edad:19));
+      lista.Add(new Persona(edad:19));
+      lista.Add(new Persona(apellido:"Morales"));
+
+      Console.Write("Nombre a cambiar: ");
+      String buscar = Console.ReadLine();
+      
+      lista.ForEach(it =>
+      {
+        if (buscar.Equals(it.Nombre))
+        {
+          Console.Write("Nuevo nombre: ");
+          it.Nombre = Console.ReadLine();
+          Console.WriteLine(it.ToString());
+        }
+
+        int añadir = 12;
+        int árbol = 14;
+      });
+    }
+  }
+}
