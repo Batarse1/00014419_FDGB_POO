92ff35825e2145f23f7873fcf0ce1006d318573a
Batarse1 <00014419@uca.edu.sv>
2020-05-01 19:34:52 -0600

    Laboratorio 6

--- Labo 06/Labo 06/CalcularNota.cs
+++ Labo 06/Labo 06/CalcularNota.cs
@@ -16,32 +16,42 @@ namespace Labo_06
                 continuar = true;
                 do 
                 {
-                       try
-                       {
-                           Console.WriteLine("¿Cúal fue su calificación de la siguiente evaluación?");
-                           if (element is Laboratorio)
-                           {
-                               Console.WriteLine("Laboratorio");
-                               Console.WriteLine(((Laboratorio)element).ToString());
-                           }
-                           else if (element is Parcial)
-                           {
-                               Console.WriteLine("Parcial");                    
-                               Console.WriteLine(((Parcial)element).ToString());
-                           }
-                           else
-                           {   
-                               Console.WriteLine("Tarea");
-                               Console.WriteLine(((Tarea)element).ToString());
-                           }
-                           opt = Convert.ToByte(Console.ReadLine());
-                           numerador += opt*element.Porcentaje;
-                           continuar = false;
-                       }
-                       catch (FormatException)
-                       {
-                           Console.WriteLine("Opción invalida,  Digite un número entre 1 y 2...");
-                       }
+                    try
+                    {
+                        Console.WriteLine("¿Cúal fue su calificación de la siguiente evaluación?");
+                        if (element is Laboratorio)
+                        {
+                            Console.WriteLine("Laboratorio");
+                            Console.WriteLine(((Laboratorio) element).ToString());
+                        }
+                        else if (element is Parcial)
+                        {
+                            Console.WriteLine("Parcial");
+                            Console.WriteLine(((Parcial) element).ToString());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Tarea");
+                            Console.WriteLine(((Tarea) element).ToString());
+                        }
+
+                        opt = Convert.ToDouble(Console.ReadLine());
+                        if (opt < 0 || opt > 10)
+                        {
+                            throw new OutOfLimitsException("Número inválido, digite un numero entre 0 y 10");
+                        }
+
+                        numerador += opt * element.Porcentaje;
+                        continuar = false;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Opción invalida,  Digite un número entre 0 y 10...");
+                    }
+                    catch (OutOfLimitsException exc)
+                    {
+                        Console.WriteLine(exc.Message);
+                    }
                 } while (continuar); 
                 cont++;
             }
